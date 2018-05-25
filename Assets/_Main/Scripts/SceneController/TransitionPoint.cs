e98f3a6c24106bc918eb8e789cf816dd25c12aac
2SMILE2 <alwayssmile11a1@gmail.com>
2018-05-25 22:04:57 +0700

    nothing

---
+++ Assets/_Main/Scripts/SceneController/TransitionPoint.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+namespace Gamekit2D
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class TransitionPoint : MonoBehaviour
+    {
+        public enum TransitionType
+        {
+            DifferentZone, DifferentNonGameplayScene, SameScene,
+        }
+
+
+        public enum TransitionWhen
+        {
+            ExternalCall, InteractPressed, OnTriggerEnter,
+        }
+
+    
+        [Tooltip("This is the gameobject that will transition.  For example, the player.")]
+        public GameObject transitioningGameObject;
+        [Tooltip("Whether the transition will be within this scene, to a different zone or a non-gameplay scene.")]
+        public TransitionType transitionType;
+        [SceneName]
+        public string newSceneName;
+        [Tooltip("The tag of the SceneTransitionDestination script in the scene being transitioned to.")]
+        public SceneTransitionDestination.DestinationTag transitionDestinationTag;
+        [Tooltip("The destination in this scene that the transitioning gameobject will be teleported.")]
+        public TransitionPoint destinationTransform;
+        [Tooltip("What should trigger the transition to start.")]
+        public TransitionWhen transitionWhen;
+    
+        bool m_TransitioningGameObjectPresent;
+
+        void Start ()
+        {
+            if (transitionWhen == TransitionWhen.ExternalCall)
+                m_TransitioningGameObjectPresent = true;
+        }
+
+        void OnTriggerEnter2D (Collider2D other)
+        {
+            if (other.gameObject == transitioningGameObject)
+            {
+                m_TransitioningGameObjectPresent = true;
+
+                if (ScreenFader.IsFading || SceneController.Transitioning)
+                    return;
+
+                if (transitionWhen == TransitionWhen.OnTriggerEnter)
+                    TransitionInternal ();
+            }
+        }
+
+        void OnTriggerExit2D (Collider2D other)
+        {
+            if (other.gameObject == transitioningGameObject)
+            {
+                m_TransitioningGameObjectPresent = false;
+            }
+        }
+
+        void Update ()
+        {
+            if (ScreenFader.IsFading || SceneController.Transitioning)
+                return;
+
+            if(!m_TransitioningGameObjectPresent)
+                return;
+
+            if (transitionWhen == TransitionWhen.InteractPressed)
+            {
+                //if (PlayerInput.Instance.Interact.Down)
+                //{
+                //    TransitionInternal ();
+                //}
+            }
+        }
+
+        protected void TransitionInternal ()
+        {
+        
+            if (transitionType == TransitionType.SameScene)
+            {
+                //GameObjectTeleporter.Teleport (transitioningGameObject, destinationTransform.transform);
+            }
+            else
+            {
+                SceneController.TransitionToScene (this);
+            }
+        }
+
+        public void Transition ()
+        {
+            if(!m_TransitioningGameObjectPresent)
+                return;
+
+            if(transitionWhen == TransitionWhen.ExternalCall)
+                TransitionInternal ();
+        }
+    }
+}

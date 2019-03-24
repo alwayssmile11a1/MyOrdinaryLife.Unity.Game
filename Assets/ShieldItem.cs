3619d5a48f52cb5ef630c6429ed41970d7f04702
ducthong230497 <15520856@gm.uit.edu.vn>
2019-03-24 12:46:08 +0700

    add Shield Item

---
+++ Assets/ShieldItem.cs
@@ -0,0 +1,17 @@
+﻿using Gamekit2D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldItem : ClickableItem
+{
+    public GameObject shieldEffect;
+
+    protected override bool OnClick(PlayerPlatformerController player)
+    {
+        //throw new System.NotImplementedException();
+        //VFXController.Instance
+        shieldEffect.SetActive(true);
+        return true;
+    }
+}

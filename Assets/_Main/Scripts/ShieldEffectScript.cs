ecb9099ae8782cf864dc54ec049ff04bb16c4cae
ducthong230497 <15520856@gm.uit.edu.vn>
2019-03-24 13:40:57 +0700

    modify shield a bit

---
+++ Assets/_Main/Scripts/ShieldEffectScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldEffectScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<CollectedBehaviour>() != null)
+        {
+            Debug.Log("Hit gem");
+        }
+    }
+}

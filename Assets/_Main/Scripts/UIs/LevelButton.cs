efd4d2f90f9aa164521af2731f4be85a409d7eb0
ducthong230497 <15520856@gm.uit.edu.vn>
2019-04-01 16:26:02 +0700

    Level Button Generator
    Fix a bug in level2-6

--- Assets/_Main/Scripts/UIs/LevelButton.cs
+++ Assets/_Main/Scripts/UIs/LevelButton.cs
@@ -33,6 +33,38 @@ public class LevelButton : MonoBehaviour {
         UIManager.Instance.AddToTotalStars(stars.Length);
     }
 
+    private void LoadStar()
+    {
+        SavedData savedData = new SavedData();
+
+        if (savedData.Load(m_SceneName))
+        {
+            for (int i = 0; i < savedData.count; i++)
+            {
+
+                stars[i].color = starActiveColor;
+            }
+
+            UIManager.Instance.AddToCurrentTotalScore(savedData.count);
+
+        }
+        UIManager.Instance.AddToTotalStars(stars.Length);
+    }
+
+    private void SetSceneName(char episodeIndex, int levelIndex)
+    {
+        m_SceneName = $"Level{episodeIndex}-{levelIndex}";
+
+        LoadStar();
+    }
+
+    public void SetSceneNameAndLoadStar(char episodeIndex, int levelIndex)
+    {
+        SetSceneName(episodeIndex, levelIndex);
+
+        LoadStar();
+    }
+
     public void LoadScene()
     {
         StartCoroutine(GameManager.Instance.LoadLevel(m_SceneName));
---
+++ Assets/_Main/Scripts/UIs/LevelButtonGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelButtonGenerator : MonoBehaviour
+{
+    public GameObject buttonPrefab;
+    public float buttonSize;
+    public Transform content;
+    public Sprite buttonBackgroundSprite;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        string[] pathSplit = SceneManager.GetActiveScene().path.Split('/');
+        string folderName = pathSplit[pathSplit.Length - 2];
+        DirectoryInfo directoryInfo = new DirectoryInfo($"Assets/_Main/_Scenes/{folderName}");
+        FileInfo[] arrFiles = directoryInfo.GetFiles("Level*.unity");
+        
+        string activeScene = SceneManager.GetActiveScene().name;
+        char episodeIndex = activeScene[activeScene.Length - 1];
+
+        for (int i = 0; i < arrFiles.Length; i++)
+        {
+            GameObject button = Instantiate(buttonPrefab, content);
+            button.GetComponent<Image>().sprite = buttonBackgroundSprite;
+            button.name = $"Level{i + 1}";
+
+            LayoutElement layoutElement = button.AddComponent<LayoutElement>();
+            layoutElement.minWidth = layoutElement.minHeight = buttonSize;
+
+            LevelButton levelButton = button.GetComponent<LevelButton>();
+            levelButton.text.text = (i + 1).ToString();
+            levelButton.SetSceneNameAndLoadStar(episodeIndex, i + 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

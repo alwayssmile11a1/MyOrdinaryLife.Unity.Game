2b5abbd4c87839c832c8868a9702b9781b693c5e
CoheeStudio <alwayssmile11a1@gmail.com>
2019-05-11 11:44:50 +0700

    CameraFitter and xARM
    CameraFitter helps fit the camera size to all screen size
    xARM is a package which helps see the game in multiple standard devices

---
+++ Assets/_Main/Scripts/HelperScripts/CameraFitter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFitter : MonoBehaviour
+{
+    public float masterCameraSize = 5;
+    public Vector2 masterAspectRatio;
+
+    void Awake()
+    {
+        FitCamera();
+    }
+
+    void FitCamera()
+    {
+        Camera camera = GetComponent<Camera>();
+        float masterWidth = masterCameraSize * 2 * masterAspectRatio.x / masterAspectRatio.y;
+        float currentAspectRatio = camera.aspect;
+        float desiredCameraSize = masterWidth / currentAspectRatio * 1 / 2;
+        camera.orthographicSize = desiredCameraSize;
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        FitCamera();
+    }
+#endif
+}
